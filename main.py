fe4ac97f085e3addbf8459c2c7a001efb051cce5
rakybond007 <rakybond007@gmail.com>
2023-07-23 22:22:35 +0900

    prehj modified add func
    Signed-off-by: rakybond007 <rakybond007@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add(a,b):
-    return a+b
+def add(add_list):
+    result = 0
+    for each_num in add_list:
+        if each_num >= 0:
+            result += each_num
+    return result
 
 def sub(a,b):
     return a-b
@@ -7,7 +11,8 @@ def sub(a,b):
 add_numbers = [1,3]
 sub_numbers = [[5,4],[4,3]]
 
-add_result = add(add_numbers[0], add_numbers[1])
+#add_result = add(add_numbers[0], add_numbers[1])
+add_result = add(add_numbers)
 sub_result1 = sub(sub_numbers[0][0], sub_numbers[1][0])
 sub_result2 = sub(sub_numbers[0][1], sub_numbers[1][1])
 
